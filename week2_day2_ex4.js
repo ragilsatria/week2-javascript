f36c9e0bfff59dd8ab8b44d9af851181afa98743
ragilsatria <satriaragil97@gmail.com>
2020-03-24 13:14:54 +0700

    excercise switch case

---
+++ week2_day2_ex4.js
@@ -0,0 +1,129 @@
+var hari = 21 
+var bulan = 1
+var tahun = 1945
+
+switch(bulan){
+    
+    case 1:
+        if ((hari >= 1 && hari <=31) && (tahun >= 1900 && tahun <= 2200)){
+            console.log(hari + ' Januari ' + tahun)
+        }
+        else{
+            console.log('data invalid')
+            console.log('input: dd/mm/yyyy - limit data 1-31/1-12/1900-2200')
+        }
+        break;
+    
+    case 2:
+        if ((hari >= 1 && hari <=31) && (tahun >= 1900 && tahun <= 2200)){
+            console.log(hari + ' Februari ' + tahun)
+        }
+        else{
+            console.log('data invalid')
+            console.log('input: dd/mm/yyyy - limit data 1-31/1-12/1900-2200')
+        }
+        break;
+
+    case 3:
+        if ((hari >= 1 && hari <=31) && (tahun >= 1900 && tahun <= 2200)){
+            console.log(hari + ' Maret ' + tahun)
+        }
+        else{
+            console.log('data invalid')
+            console.log('input: dd/mm/yyyy - limit data 1-31/1-12/1900-2200')
+        }
+        break;
+        
+    case 4:
+        if ((hari >= 1 && hari <=31) && (tahun >= 1900 && tahun <= 2200)){
+            console.log(hari + ' April ' + tahun)
+        }
+        else{
+            console.log('data invalid')
+            console.log('input: dd/mm/yyyy - limit data 1-31/1-12/1900-2200')
+        }
+        break;
+        
+    case 5:
+        if ((hari >= 1 && hari <=31) && (tahun >= 1900 && tahun <= 2200)){
+            console.log(hari + ' Mei ' + tahun)
+        }
+        else{
+            console.log('data invalid')
+            console.log('input: dd/mm/yyyy - limit data 1-31/1-12/1900-2200')
+        } 
+        break;   
+        
+    case 6:
+        if ((hari >= 1 && hari <=31) && (tahun >= 1900 && tahun <= 2200)){
+            console.log(hari + ' Juni ' + tahun)
+        }
+        else{
+            console.log('data invalid')
+            console.log('input: dd/mm/yyyy - limit data 1-31/1-12/1900-2200')
+        }
+        break;
+    
+    case 7:
+        if ((hari >= 1 && hari <=31) && (tahun >= 1900 && tahun <= 2200)){
+            console.log(hari + ' Juli ' + tahun)
+        }
+        else{
+            console.log('data invalid')
+            console.log('input: dd/mm/yyyy - limit data 1-31/1-12/1900-2200')
+        }
+        break;
+
+    case 8:
+        if ((hari >= 1 && hari <=31) && (tahun >= 1900 && tahun <= 2200)){
+            console.log(hari + ' Agustus ' + tahun)
+        }
+        else{
+            console.log('data invalid')
+            console.log('input: dd/mm/yyyy - limit data 1-31/1-12/1900-2200')
+        }
+        break;
+        
+    case 9:
+        if ((hari >= 1 && hari <=31) && (tahun >= 1900 && tahun <= 2200)){
+            console.log(hari + ' September ' + tahun)
+        }
+        else{
+            console.log('data invalid')
+            console.log('input: dd/mm/yyyy - limit data 1-31/1-12/1900-2200')
+        }
+        break;
+        
+    case 10:
+        if ((hari >= 1 && hari <=31) && (tahun >= 1900 && tahun <= 2200)){
+            console.log(hari + ' Oktober ' + tahun)
+        }
+        else{
+            console.log('data invalid')
+            console.log('input: dd/mm/yyyy - limit data 1-31/1-12/1900-2200')
+        }
+        break;
+        
+    case 11:
+        if ((hari >= 1 && hari <=31) && (tahun >= 1900 && tahun <= 2200)){
+            console.log(hari + ' November ' + tahun)
+        }
+        else{
+            console.log('data invalid')
+            console.log('input: dd/mm/yyyy - limit data 1-31/1-12/1900-2200')
+        }
+        break;
+        
+    case 12:
+        if ((hari >= 1 && hari <=31) && (tahun >= 1900 && tahun <= 2200)){
+            console.log(hari + ' Desember ' + tahun)
+        }
+        else{
+            console.log('data invalid')
+            console.log('input: dd/mm/yyyy - limit data 1-31/1-12/1900-2200')
+        }
+        break;
+    
+    default:
+        console.log('input: dd/mm/yyyy - limit data 1-31/1-12/1900-2200')
+}
